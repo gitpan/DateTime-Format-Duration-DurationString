24166c20ca59c444463289aca2184d8e32e6d40b
Bjørn-Olav Strand <bolav@cpan.org>
2009-07-14 03:26:17 +0000

    Import of BOLAV/DateTime-Format-Duration-DurationString-0.02 from CPAN.
    gitpan-cpan-distribution: DateTime-Format-Duration-DurationString
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BOLAV/DateTime-Format-Duration-DurationString-0.02.tar.gz
    gitpan-cpan-author:       BOLAV
    gitpan-cpan-maturity:     released

--- lib/DateTime/Format/Duration/DurationString.pm
+++ lib/DateTime/Format/Duration/DurationString.pm
@@ -10,11 +10,11 @@ DateTime::Format::Duration::DurationString - JIRA style parsing of duration
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
